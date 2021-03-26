808c8a790c21a7da1db79f4dba284d3d48f80e4e
Kid <kidonng@gmail.com>
2021-03-26 23:02:17 +0800

    Improve `rzf` and adjust Fish scripts

---
+++ .config/fish/custom/completions/zoxide.fish
@@ -0,0 +1,3 @@
+complete -c zoxide -x -n __fish_use_subcommand -a "add help import init query remove"
+complete -c zoxide -x -s h -l help -d "Help"
+complete -c zoxide -x -s V -l version -d "Version"
--- .config/fish/custom/functions/rzf.fish
+++ .config/fish/custom/functions/rzf.fish
@@ -1,9 +1,25 @@
 # Based on https://github.com/junegunn/fzf/issues/1598#issuecomment-719573480
 function rzf
-    rg --color=always --line-number --smart-case "$argv" |
+    argparse -i e/editor -- $argv
+
+    set selection (rg --color=always --line-number --smart-case "$argv" |
     fzf --ansi --delimiter : \
-      --with-nth=1,3 \
-      --preview 'bat --style=numbers --color=always --highlight-line {2} {1}' \
-      --preview-window +{2}-/2 |
-    string split : -f 1
+        --with-nth=1,3 \
+        --preview 'bat --style=numbers --color=always --highlight-line {2} {1}' \
+        --preview-window +{2}-/2)
+
+    test -z "$selection" && return
+
+    if set -q _flag_editor
+        string split -f 1,2 : $selection | read -z file line
+        $EDITOR +$line $file
+        return
+    end
+
+    set selection (string split -f 1 : $selection)
+    if status is-command-substitution
+        echo $selection
+    else
+        commandline --current-token --replace -- (string escape -- $selection | string join ' ')
+    end
 end
