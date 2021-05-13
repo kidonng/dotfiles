498ea30b966c4fd61e523ced95611f47f5fd5aac
Kid <kidonng@gmail.com>
2021-05-13 22:07:49 +0800

    Use tide prompt

--- .config/fish/functions/fish_title.fish
+++ .config/fish/functions/fish_title.fish
@@ -1,11 +1,5 @@
 # Based on $__fish_data_dir/functions/fish_title.fish
 function fish_title
-    if not set -q INSIDE_EMACS; or string match -vq '*,term:*' -- $INSIDE_EMACS
-        set command (set -q argv[1] && echo $argv[1] || status current-command)
-        if test "$command" = fish
-            string replace $HOME "~" (__fish_pwd)
-        else
-            echo $command
-        end
-    end
+    set -q argv[1] && echo "$argv · "
+    string replace $HOME "~" $PWD
 end
